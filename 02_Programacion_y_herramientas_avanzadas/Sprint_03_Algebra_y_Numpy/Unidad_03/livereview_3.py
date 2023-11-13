4939685ea37e8beaca8ff1279e65f41e694748af
Ángela López Molina <147253096+AngelaLM14@users.noreply.github.com>
2023-11-13 08:58:08 +0100

    Sprint 4 - unidad 1

---
+++ 02_Programacion_y_herramientas_avanzadas/Sprint_03_Algebra_y_Numpy/Unidad_03/livereview_3.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+TAM_TABLERO = (10,10)
+
+def crea_tablero(dimensiones=TAM_TABLERO, tile = " "):
+    return np.full(dimensiones,tile)
+
+dimensiones_usuario = input(f"De cuánto quieres el tablero (alto,ancho) ({TAM_TABLERO})")
+
+dim_val = [int (valor) for valor in dimensiones_usuario.split(",")]
+
+
+print(crea_tablero(dim_val))
+
